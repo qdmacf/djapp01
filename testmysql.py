1808fa431b7365f3cc7f5c356f9a365a41c61b47
mark <machf@coscon.com>
2020-02-06 23:42:22 +0800

    win-modify 1st

--- testmysql.py
+++ testmysql.py
@@ -1,6 +1,7 @@
 import pymysql
 # 打开数据库连接
-conn = pymysql.connect('rm-m5evc6d1di064l8d4125010.mysql.rds.aliyuncs.com', 'machf', 'Super2020', 'vsmain')
+# conn = pymysql.connect('rm-m5evc6d1di064l8d4125010.mysql.rds.aliyuncs.com', 'machf', 'Super2020', 'vsmain')
+conn = pymysql.connect('rm-m5evc6d1di064l8d4ko.mysql.rds.aliyuncs.com', 'machf', 'Super2020', 'vsmain')
 # 使用cursor()方法创建一个游标对象
 cursor = conn.cursor()
 
